74894638bffdcea7ae1bd0c2fea8b2d90c18641b
ray xiao <rayxiao@MacBooks-MacBook-Pro.local>
2023-08-02 15:52:29 -0400

    enhance report

--- src/main/java/com/xmllab/validator/XmlValidator.java
+++ src/main/java/com/xmllab/validator/XmlValidator.java
@@ -1,5 +1,8 @@
 package com.xmllab.validator;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
@@ -12,6 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class XmlValidator {
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+
   public static void main(String[] args) {
     if (args.length != 1) {
       System.err.println("Usage: XmlValidator <dir_path>");
@@ -39,8 +44,7 @@ public class XmlValidator {
   }
 
   private static void validateXmlFile(File xmlFile) {
-    System.out.println("Processing file: " + xmlFile.getAbsolutePath());
-
+    List<String> errors = new ArrayList<>();
     try {
       // Enable validation
       SAXParserFactory spf = SAXParserFactory.newInstance();
@@ -51,31 +55,19 @@ public class XmlValidator {
 
       // Define an error handler to catch any problems during parsing
       DefaultHandler handler = new DefaultHandler() {
-        private List<SAXParseException> errors = new ArrayList<>();
-
         @Override
         public void warning(SAXParseException e) {
-          errors.add(e);
+          errors.add(e.getMessage());
         }
 
         @Override
         public void error(SAXParseException e) {
-          errors.add(e);
+          errors.add(e.getMessage());
         }
 
         @Override
         public void fatalError(SAXParseException e) {
-          errors.add(e);
-        }
-
-        @Override
-        public void endDocument() {
-          if (!errors.isEmpty()) {
-            System.out.println("Validation failed with " + errors.size() + " error(s):");
-            errors.forEach(e -> System.out.println(e.getMessage()));
-          } else {
-            System.out.println("Validation successful!");
-          }
+          errors.add(e.getMessage());
         }
       };
 
@@ -83,7 +75,26 @@ public class XmlValidator {
       saxParser.parse(xmlFile, handler);
 
     } catch (SAXException | IOException | ParserConfigurationException e) {
-      System.out.println("An unexpected error occurred: " + e.getMessage());
+      errors.add(e.getMessage());
+    }
+
+    ObjectNode result = objectMapper.createObjectNode();
+    result.put("name", xmlFile.getAbsolutePath());
+
+    if (errors.isEmpty()) {
+      result.put("status", "success");
+    } else {
+      result.put("status", "failed");
+      ArrayNode errorsNode = objectMapper.createArrayNode();
+      errors.forEach(errorsNode::add);
+      result.set("errors", errorsNode);
+    }
+
+    try {
+      String jsonOutput = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(result);
+      System.out.println(jsonOutput);
+    } catch (Exception e) {
+      System.out.println("Error creating JSON output: " + e.getMessage());
     }
   }
 }
