faab8201798329b20a339bd6a9555c457b0c437f
ray xiao <rayxiao@MacBooks-MacBook-Pro.local>
2023-08-02 16:09:20 -0400

    export html report

--- src/main/java/com/xmllab/validator/XmlValidator.java
+++ src/main/java/com/xmllab/validator/XmlValidator.java
@@ -1,25 +1,28 @@
 package com.xmllab.validator;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import javax.xml.parsers.ParserConfigurationException;
-import org.xml.sax.*;
-import org.xml.sax.helpers.DefaultHandler;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class XmlValidator {
+
   private static final ObjectMapper objectMapper = new ObjectMapper();
+  private static String outputFormat = "html";
 
   public static void main(String[] args) {
-    if (args.length != 1) {
-      System.err.println("Usage: XmlValidator <dir_path>");
+    if (args.length < 1 || args.length > 2) {
+      System.err.println("Usage: XmlValidator <dir_path> [-json | -html]");
       System.exit(1);
     }
 
@@ -28,6 +31,14 @@ public class XmlValidator {
     if (!directory.exists() || !directory.isDirectory()) {
       System.err.println("Invalid directory path: " + args[0]);
       System.exit(1);
+    }
+
+    if (args.length == 2) {
+      if (!args[1].equals("-json") && !args[1].equals("-html")) {
+        System.err.println("Invalid output format: " + args[1]);
+        System.exit(1);
+      }
+      outputFormat = args[1].substring(1);
     }
 
     processDirectory(directory);
@@ -91,10 +102,39 @@ public class XmlValidator {
     }
 
     try {
-      String jsonOutput = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(result);
-      System.out.println(jsonOutput);
+      if (outputFormat.equals("json")) {
+        String jsonOutput = objectMapper.writerWithDefaultPrettyPrinter()
+            .writeValueAsString(result);
+        System.out.println(jsonOutput);
+      } else {
+        String htmlOutput = createHtmlOutput(objectMapper.readTree(result.toString()));
+        System.out.println(htmlOutput);
+      }
     } catch (Exception e) {
-      System.out.println("Error creating JSON output: " + e.getMessage());
+      System.out.println("Error creating output: " + e.getMessage());
     }
+
+
+  }
+
+  private static String createHtmlOutput(JsonNode jsonNode) {
+    StringBuilder html = new StringBuilder();
+
+    html.append("<details><summary>").append(jsonNode.get("name").asText()).append("</summary>");
+    html.append("<p>Status: ").append(jsonNode.get("status").asText()).append("</p>");
+
+    if (jsonNode.get("status").asText().equals("failed")) {
+      html.append("<details><summary>Errors</summary><ul>");
+
+      for (JsonNode error : jsonNode.get("errors")) {
+        html.append("<li>").append(error.asText()).append("</li>");
+      }
+
+      html.append("</ul></details>");
+    }
+
+    html.append("</details>");
+
+    return html.toString();
   }
 }
