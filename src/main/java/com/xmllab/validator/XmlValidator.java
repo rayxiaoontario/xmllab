eaf6fed56503748fb57360fc304e480747931c43
ray xiao <rayxiao@MacBooks-MacBook-Pro.local>
2023-08-02 15:08:04 -0400

    single assemble

--- src/main/java/com/xmllab/validator/XmlValidator.java
+++ src/main/java/com/xmllab/validator/XmlValidator.java
@@ -1,12 +1,11 @@
 package com.xmllab.validator;
 
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
+import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
-import java.io.IOException;
 
 public class XmlValidator {
   public static void main(String[] args) {
@@ -19,33 +18,51 @@ public class XmlValidator {
     String dtdFile = args[1];
 
     try {
-      // Create a new XMLReader
-      XMLReader reader = new org.apache.xerces.parsers.SAXParser();
+      // Enable validation
+      SAXParserFactory spf = SAXParserFactory.newInstance();
+      spf.setValidating(true);
 
-      // Activate validation
-      reader.setFeature("http://xml.org/sax/features/validation", true);
+      // Get a new instance of parser
+      SAXParser saxParser = spf.newSAXParser();
 
-      // Load external DTD
-      reader.setProperty("http://apache.org/xml/properties/schema/external-noNamespaceSchemaLocation", "file://" + dtdFile);
+      // Set the XMLReader's DTD handling properties
+      XMLReader xmlReader = saxParser.getXMLReader();
+      xmlReader.setEntityResolver(new EntityResolver() {
+        @Override
+        public InputSource resolveEntity(String publicId, String systemId) {
+          if (systemId.endsWith(".dtd")) {
+            return new InputSource(dtdFile);
+          } else {
+            return null;
+          }
+        }
+      });
 
-      // Error handler to report errors and warnings
-      DefaultHandler errorHandler = new DefaultHandler() {
-        public void error(SAXException e) throws SAXException {
+      // Define an error handler to catch any problems during parsing
+      xmlReader.setErrorHandler(new ErrorHandler() {
+        @Override
+        public void warning(SAXParseException e) throws SAXException {
+          throw e; // Throwing exception on warnings to make validation strict
+        }
+
+        @Override
+        public void error(SAXParseException e) throws SAXException {
           throw e;
         }
-      };
-      reader.setErrorHandler(errorHandler);
 
-      // Parse the document
-      reader.parse(new InputSource(xmlFile));
+        @Override
+        public void fatalError(SAXParseException e) throws SAXException {
+          throw e;
+        }
+      });
+
+      // Parse the document to validate
+      xmlReader.parse(new InputSource(xmlFile));
 
       System.out.println("Validation successful!");
 
-    } catch (SAXException e) {
+    } catch (Exception e) {
       System.out.println("Validation failed: " + e.getMessage());
-      System.exit(1);
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
       System.exit(1);
     }
   }
